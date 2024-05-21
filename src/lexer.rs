369926cc730f070e9c039b88eec502132e46c00f
TogarashiPepper <83902606+TogarashiPepper@users.noreply.github.com>
2024-05-20 20:08:55 -0700

    feat(lexer): add minus, times, slash and add a builder for Token

--- src/lexer.rs
+++ src/lexer.rs
@@ -4,6 +4,9 @@ use std::ops::Range;
 pub enum TokenType {
     PiTimes,
     Plus,
+    Minus,
+    Times,
+    Slash
 }
 
 #[derive(Debug, PartialEq, Clone)]
@@ -11,6 +14,60 @@ pub struct Token<'a> {
     variant: TokenType,
     location: Range<usize>,
     lexeme: &'a str,
+}
+
+pub struct TokenBuilder<'a> {
+    variant: Option<TokenType>,
+    idx: Option<usize>,
+    chr: Option<char>,
+    input_string: &'a str,
+}
+
+impl<'a> TokenBuilder<'a> {
+    fn new(input: &'a str) -> TokenBuilder<'a> {
+        TokenBuilder {
+            variant: None,
+            idx: None,
+            chr: None,
+            input_string: input,
+        }
+    }
+
+    fn variant(mut self, variant: TokenType) -> TokenBuilder<'a> {
+        self.variant = Some(variant);
+        self
+    }
+
+    fn chr(mut self, chr: char) -> TokenBuilder<'a> {
+        self.chr = Some(chr);
+
+        self
+    }
+
+    fn idx(mut self, idx: usize) -> TokenBuilder<'a> {
+        self.idx = Some(idx);
+
+        self
+    }
+
+    fn build(mut self) -> Token<'a> {
+        if self.variant.is_some() && self.idx.is_some() && self.chr.is_some() {
+            let idx = self.idx.unwrap();
+
+            let variant = self.variant.unwrap();
+            let location = idx..idx + self.chr.unwrap().len_utf8();
+            let lexeme = &self.input_string[location.clone()];
+
+            Token {
+                variant,
+                location,
+                lexeme,
+            }
+        }
+        else {
+            panic!("Attempted to build builder without all of the fields filled out")
+        }
+    }
 }
 
 #[derive(Debug, PartialEq)]
@@ -25,18 +82,16 @@ pub fn lex<'a>(input: &'a str) -> Result<Vec<Token<'a>>, LexError> {
     for (idx, c) in it {
         use TokenType::*;
 
+        let builder = TokenBuilder::new(input).chr(c).idx(idx);
+// idx..idx + x.len_utf8()
+
         // TODO: Make this code more DRY, perhaps a macro or some kind of helper?
         let token = match c {
-            x @ '○' => Token {
-                variant: PiTimes,
-                location: idx..idx + x.len_utf8(),
-                lexeme: &input[idx..idx + x.len_utf8()],
-            },
-            x @ '+' => Token {
-                variant: Plus,
-                location: idx..idx + x.len_utf8(),
-                lexeme: &input[idx..idx + x.len_utf8()],
-            },
+            '○' => builder.variant(PiTimes).build(),
+            '+' => builder.variant(Plus).build(),
+            '-' => builder.variant(Minus).build(),
+            '*' => builder.variant(Times).build(),
+            '/' => builder.variant(Slash).build(),
             _ => return Err(LexError::InvalidToken(input[idx..=idx].to_string())),
         };
 
@@ -78,5 +133,20 @@ mod tests {
     #[test]
     fn lex_plus() {
         lex_single("+", Plus);
+    }
+
+    #[test]
+    fn lex_minus() {
+        lex_single("-", Minus);
+    }
+
+    #[test]
+    fn lex_times() {
+        lex_single("*", Times);
+    }
+
+    #[test]
+    fn lex_slash() {
+        lex_single("/", Slash);
     }
 }
