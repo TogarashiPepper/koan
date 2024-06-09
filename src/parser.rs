a0c20515fc066ad5da9bede359207c54875a8ef5
TogarashiPepper <83902606+TogarashiPepper@users.noreply.github.com>
2024-06-09 16:35:45 -0700

    chore: fix comment

--- src/parser.rs
+++ src/parser.rs
@@ -1,56 +1,53 @@
-use crate::lexer::{TokenType, Token};
+use std::iter::Peekable;
 
-enum Operator {
-    PiTimes,
-    Plus,
-    Minus,
-    Times,
-    Slash,
-    Assign,
-    Equality,
-    Greater,
-    GreaterEqual,
-    Lesser,
-    LesserEqual,
-}
+use crate::lexer::{Token, TokenType};
+
+/// Wrapper around TokenType that ensures its an operator token, rather than a quote or literal
+struct Operator(TokenType);
 
 impl TryFrom<TokenType> for Operator {
     type Error = &'static str;
 
     fn try_from(value: TokenType) -> Result<Self, Self::Error> {
         Ok(match value {
-            TokenType::PiTimes => Self::PiTimes,
-            TokenType::Plus => Self::Plus,
-            TokenType::Minus => Self::Minus,
-            TokenType::Times => Self::Times,
-            TokenType::Slash => Self::Slash,
-            TokenType::Equal => Self::Assign,
-            TokenType::DoubleEqual => Self::Equality,
-            TokenType::Greater => Self::Greater,
-            TokenType::GreaterEqual => Self::GreaterEqual,
-            TokenType::Lesser => Self::Lesser,
-            TokenType::LesserEqual => Self::LesserEqual,
-            _ => return Err("The provided TokenType is not an operator")
+            x @ (TokenType::PiTimes
+            | TokenType::Plus
+            | TokenType::Minus
+            | TokenType::Times
+            | TokenType::Slash
+            | TokenType::Equal
+            | TokenType::DoubleEqual
+            | TokenType::Greater
+            | TokenType::GreaterEqual
+            | TokenType::Lesser
+            | TokenType::LesserEqual) => Self(x),
+            _ => return Err("The provided TokenType is not an operator"),
         })
     }
 }
 
-enum Ast {
-
-}
+enum Ast {}
 
 enum Expr {
     // Convert this to BinOp(BinOp) for impls on it?
     BinOp {
         lhs: Box<Expr>,
         op: Operator,
-        rhs: Box<Expr>
+        rhs: Box<Expr>,
     },
     Ident(String),
-    // TODO: Add integer literals to avoid JS-esque strangeness
+    // TODO: Add typed integer literals to avoid JS-esque strangeness
     NumLit(f64),
 }
 
-pub fn parse<'a>(tokens: Vec<Token<'a>>) -> Ast {
+pub fn parse(tokens: Vec<Token<'_>>) -> Ast {
+    todo!()
+}
+
+pub fn expr(tokens: Vec<Token<'_>>) -> Expr {
+    expr_bp(tokens.into_iter().peekable())
+}
+
+fn expr_bp<'a>(it: Peekable<impl Iterator<Item = Token<'a>>>) -> Expr {
     todo!()
 }
