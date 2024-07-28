519589c727de1aa16b300ed8089ab0ae7f7c48db
TogarashiPepper <83902606+TogarashiPepper@users.noreply.github.com>
2024-07-27 17:10:25 -0700

    feat: rudimentary ast type inference

--- src/inferance.rs
+++ src/inferance.rs
@@ -1,31 +1,127 @@
 use std::collections::HashMap;
 
 use crate::{
-    error::{InterpError, Result}, lexer::Operator, parser::Ast, pool::{Expr, ExprPool, ExprRef}, state::Function, value::ValTy
+    error::{InterpError, Result},
+    lexer::Operator,
+    parser::Ast,
+    pool::{Expr, ExprPool, ExprRef},
+    state::{Function, State},
+    value::ValTy,
 };
 
-
+// TODO: replace this with State and give state a generic param
 #[derive(Debug)]
 pub struct StateSim {
     // Array of environments, index = depth (i.e. `0` = global scope)
-    pub variables: Vec<HashMap<String, Option<ValTy>>>,
+    pub variables: Vec<HashMap<String, ValTy>>,
     pub functions: HashMap<String, Function>,
 }
 
-/// Performs type inference on the provided Ast
-pub fn infer(ast: Ast, pool: ExprPool) -> Ast {
-    ast
+impl StateSim {
+    fn get(&self, k: &str) -> Option<ValTy> {
+        for scope in self.variables.iter().rev() {
+            if let Some(x) = scope.get(k) {
+                return Some(x.clone());
+            }
+        }
+
+        None
+    }
+
+    fn set(&mut self, k: String, v: ValTy) {
+        // Unwrap is okay bc we don't let length fall <1
+        let x = self.variables.last_mut().unwrap();
+        x.insert(k, v);
+    }
 }
 
-pub fn infer_exp(expr: ExprRef, pool: &ExprPool) -> Result<ValTy> {
+/// Performs type inference on the provided Ast
+pub fn infer(
+    ast: &Ast,
+    pool: &ExprPool,
+    state_sim: &mut StateSim,
+) -> Result<ValTy> {
+    match ast {
+        Ast::Expression(e) => infer_exp(e, pool, todo!()),
+        Ast::Statement(_) => Ok(ValTy::Nothing),
+        Ast::Block(statements) => {
+            state_sim.variables.push(HashMap::new());
+            let len = statements.len();
+
+            if len >= 2 {
+                for stmt in &statements[0..len - 2] {
+                    infer(stmt, pool, state_sim);
+                }
+            }
+
+            if len >= 1 {
+                let ret_val = &statements[len - 1];
+
+                infer(ret_val, pool, state_sim)
+            } else {
+                Ok(ValTy::Nothing)
+            }
+        }
+        Ast::LetDecl { name, ty, body } => {
+            let infered = infer_exp(*body, pool, todo!())?;
+            state_sim.set(name.to_owned(), infered);
+
+            Ok(infered)
+        }
+        Ast::FunDecl { name, params, body } => todo!(),
+    }
+}
+
+pub fn annotate(ast: Ast, pool: &ExprPool) -> Result<Ast> {
+    let mut global = HashMap::new();
+    global.insert("π".to_owned(), ValTy::Number);
+    global.insert("e".to_owned(), ValTy::Number);
+
+    let mut state_sim = StateSim {
+        variables: vec![global],
+        functions: HashMap::new(),
+    };
+
+    match ast {
+        Ast::Expression(_) | Ast::Statement(_) | Ast::Block(_) => Ok(ast),
+        Ast::LetDecl {
+            ref name,
+            ty: _,
+            body,
+        } => {
+            let inferred = infer(&ast, pool, &mut state_sim)?;
+            let annotated = Ast::LetDecl {
+                name: name.to_owned(),
+                ty: Some(inferred),
+                body,
+            };
+
+            Ok(annotated)
+        }
+        Ast::FunDecl { name, params, body } => todo!(),
+    }
+}
+
+/// `infer_exp` is used to perform type inference on an `Expr`. The resolver is a function that takes an
+/// identifier and infers the type of the value held by that idenftifier. This parameter is used to
+/// allow the inference for the AST to provide the `Expr` the information it needs without
+/// providing it the entire scope tree every time.
+pub fn infer_exp<F>(
+    expr: ExprRef,
+    pool: &ExprPool,
+    resolve: &mut F,
+) -> Result<ValTy>
+where
+    F: FnMut(String) -> Result<ValTy>,
+{
     use ValTy::{Array, Number, String};
 
     let exp = pool.get(expr);
 
     Ok(match exp {
         Expr::BinOp { lhs, op, rhs } => {
-            let lhs = infer_exp(*lhs, pool)?;
-            let rhs = infer_exp(*rhs, pool)?;
+            let lhs = infer_exp(*lhs, pool, resolve)?;
+            let rhs = infer_exp(*rhs, pool, resolve)?;
 
             match op {
                 Operator::Power => match (lhs, rhs) {
@@ -96,7 +192,7 @@ pub fn infer_exp(expr: ExprRef, pool: &ExprPool) -> Result<ValTy> {
             }
         }
         Expr::PreOp { op: _, rhs } => {
-            let rhs = infer_exp(*rhs, pool)?;
+            let rhs = infer_exp(*rhs, pool, resolve)?;
             match rhs {
                 Number => Number,
                 Array => Array,
@@ -113,7 +209,7 @@ pub fn infer_exp(expr: ExprRef, pool: &ExprPool) -> Result<ValTy> {
         Expr::NumLit(_) => Number,
         Expr::StrLit(_) => String,
         Expr::Array(_) => Array,
-        Expr::Ident(_) => todo!(),
+        Expr::Ident(name) => resolve(name.to_owned())?,
         Expr::FunCall(_, _) => todo!(),
         Expr::IfElse {
             cond,
